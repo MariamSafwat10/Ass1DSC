70f957a5b69422df764919a005815a274c828f38
MariamSafwat6 <mariamkharoubb@gmail.com>
2025-11-23 20:52:35 +0200

    Week2

--- Ass1DSC/Program.cs
+++ Ass1DSC/Program.cs
@@ -4,7 +4,103 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //Console.Write("Enter number : ");
+            //int num = int.Parse(Console.ReadLine());
+            //for(int i = 0; i <= 12; i++)
+            //{
+            //    Console.WriteLine($"{i} * {num} = {i*num}");
+            //}
+            #endregion
+
+            #region Q2
+            //Console.Write("Enter number : ");
+            //int num = int.Parse(Console.ReadLine());
+            //int result = 0;
+            //for(int i = 0; i < num; i++)
+            //{
+            //    result += num;
+            //}
+            //Console.WriteLine($"Result : {result}");
+            #endregion
+
+            #region Q3
+            //Console.Write("How many numbers you want to enter ? ");
+            //int num = int.Parse(Console.ReadLine());
+            //int evenC = 0;
+            //int oddC = 0;
+            //int number;
+            //for (int i = 0; i < num; i++)
+            //{
+            //    number = int.Parse(Console.ReadLine());
+            //    if (number % 2 == 0)
+            //    {
+            //        evenC++;
+            //    }
+            //    else
+            //    {
+            //        oddC++;
+            //    }
+            //}
+            //Console.WriteLine($"{evenC} even numbers , {oddC} odd numbers ");
+            #endregion
+
+            #region Q4
+            //Console.WriteLine("Enter ten numbers : ");
+            //int num;
+            //int valid = 0;
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    num = int.Parse(Console.ReadLine());
+            //    if(num < 0)
+            //    {
+            //        Console.WriteLine("Negative numbers are skipped.");
+            //        continue;                  
+            //    }
+            //    else if(num == 999)
+            //    {
+            //        Console.WriteLine("Stop signal received. Ending input early.");
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        valid += num;
+            //    }
+            //}
+            //Console.WriteLine($"Sum of valid numbers = {valid}");
+            #endregion
+
+            #region Q4
+            //Console.WriteLine("Enter size of array : ");
+            //int size = int.Parse(Console.ReadLine());
+            //int[] arr = new int[size];
+            //Console.WriteLine("Enter array numbers : ");
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //}
+    
+            //Console.Write("Target : ");
+            //int target = int.Parse(Console.ReadLine());
+
+            //bool f = false;
+            //for(int i = 0; i < arr.Length; i++)
+            //{
+            //    for(int j = i + 1; j < arr.Length; j++)
+            //    {
+            //        if(arr[i] + arr[j] == target)
+            //        {
+            //            Console.WriteLine($"Indices : [{i} , {j}]");
+            //            f = true;
+            //        }
+            //    }
+                
+            //}
+            //if (!f)
+            //{
+            //    Console.WriteLine("Target doesn't achieved.");
+            //}
+            #endregion
         }
     }
 }
